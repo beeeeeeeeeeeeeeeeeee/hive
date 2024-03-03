566bfac75c1aece3daecd4cee1ea446ed6f423cb
Jer <58816840+JeremyEspresso@users.noreply.github.com>
2024-03-03 11:42:30 +0200

    🐝

---
+++ src/Hive.Data/Entities/EmailRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Hive.Data.Entities;
+
+public class EmailRequest
+{
+    public int Id { get; set; }
+
+    public string Host { get; set; }
+
+    public ulong Size { get; set; }
+
+    public string TextContent { get; set; }
+}
---
+++ src/Hive.Data/Entities/HttpRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Hive.Data.Entities;
+
+public class HttpRequest
+{
+    public int Id { get; set; }
+
+    public string Method { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public ulong Size { get; set; }
+
+    public Dictionary<string, string> Headers { get; set; }
+
+    public Dictionary<string, string> QueryStrings { get; set; }
+
+    public Dictionary<string, string> FormValues { get; set; }
+
+}
